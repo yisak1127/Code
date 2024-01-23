7d76485ce0cd8dd313e4eb95e2924efa35d00603
Yisak1127 <yusak1127@gmail.com>
2024-01-23 13:29:49 -0600

    add 2336

---
+++ 2336/ComputeArea.java
@@ -0,0 +1,11 @@
+public class ComputeArea {
+
+    public static void main(String[] args) {
+        double radius;
+        double area;
+    
+        radius=20;
+
+        
+    }
+}
---
+++ 2336/ComputeAreaWithConsoleInput.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class ComputeAreaWithConsoleInput {
+    public static void main{String[] args}{
+        
+    }
+}
